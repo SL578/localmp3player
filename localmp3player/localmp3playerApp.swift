efed90c602dcf9f354899b99967c962cc589394c
Stephen Lin <linstephen578@gmail.com>
2026-08-23 20:38:04 -0700

    Initial Commit

---
+++ localmp3player/localmp3playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  localmp3playerApp.swift
+//  localmp3player
+//
+//  Created by Stephen Lin on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct localmp3playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
